4524aee5d552b541be35bb715f7765d597b29ec6
Luis Velastegui <luisvelastegui397@gmail.com>
2021-02-12 16:28:32 -0500

    reporte venta query

--- Controllers/ReporteController.cs
+++ Controllers/ReporteController.cs
@@ -19,11 +19,13 @@ namespace Tach.Controllers
 
         [HttpGet]
         public async Task<IActionResult> Get() {
-            var categorias = await _context.Categorias.Where("Estado == true && EstadoTabla == true")
-                .OrderBy("Descripcion").Select<Categoria>("new(Id, Descripcion, Repuestos.Count as Stock)").ToListAsync();
-            var marcas = await _context.Marcas.Where("Estado == true && EstadoTabla == true")
-                .OrderBy("Descripcion").Select<Marca>("new(Id, Descripcion, Repuestos.Count as Stock)").ToListAsync();
-            return Ok(new { marcas = marcas, categorias = categorias });
+            var categorias = await _context.Categorias.Where("Estado == true && EstadoTabla == true").OrderBy("Repuestos.Count desc")
+                .Take(5).Select<Categoria>("new(Id, Descripcion, Repuestos.Count as Stock)").ToListAsync();
+            var marcas = await _context.Marcas.Where("Estado == true && EstadoTabla == true").OrderBy("Repuestos.Count desc")
+                .Take(5).Select<Marca>("new(Id, Descripcion, Repuestos.Count as Stock)").ToListAsync();
+            var ventas = await _context.Ventas.Where("Estado == true").OrderBy("FechaIngreso")
+                .Select("new(Cantidad,Total,FechaIngreso)").ToDynamicArrayAsync();
+            return Ok(new { marcas = marcas, categorias = categorias, ventas = ventas });
         }
     }
 }
